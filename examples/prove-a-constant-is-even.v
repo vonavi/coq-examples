ea76d613d5aef088b7d3742877c977342074b4a6
vonavi <vonavi@users.noreply.github.com>
2021-11-18 05:21:26 +0000

    deploy: 58be92b1da7e616f4ffdb69b97d1e62df0479180

---
+++ examples/prove-a-constant-is-even.v
@@ -0,0 +1,60 @@
+(*|
+Prove a constant is even
+========================
+
+:Link: https://stackoverflow.com/questions/62326896/prove-a-constant-is-even
+|*)
+
+(*|
+Question
+--------
+
+Given the inductive definition of evenness, how is it best proved
+that, say, 1024 is an even number? Repeating `apply even_S` down to
+zero is certainly not the right approach.
+|*)
+
+(*|
+Answer (HTNW)
+-------------
+
+Repeating `apply even_S` is not the way. `repeat apply even_S` is. If
+`even_S` is a constructor, there's also `repeat constructor`.
+|*)
+
+Inductive even : nat -> Prop :=
+| even_O : even O
+| even_S : forall n, even n -> even (S (S n)).
+
+Goal (even 1024). repeat apply even_S. exact even_O. Qed.
+Goal (even 1024). repeat constructor. Qed.
+(* also finds even_O, would leave as goal otherwise *)
+
+(*|
+Answer (Arthur Azevedo De Amorim)
+---------------------------------
+
+As HTNW pointed out, you can use Ltac automation to produce such a
+proof. This has the disadvantage of producing a large proof term
+`even_S (even_S ... even_O)`, thus slowing down proofs. In this case,
+it is better to reformulate the goal using a boolean decision
+procedure:
+|*)
+
+Fixpoint evenb (n : nat) : bool :=
+  match n with
+  | 0 => true
+  | 1 => false
+  | S (S m) => evenb m
+  end.
+
+Lemma evenb_correct : forall n, evenb n = true <-> even n.
+(* Fun exercise! *)
+Admitted. (* .none *)
+
+(*|
+Coq can prove that `evenb 1024 = true` simply by evaluating the
+left-hand side:
+|*)
+
+Goal (even 1024). apply evenb_correct. reflexivity. Qed.
