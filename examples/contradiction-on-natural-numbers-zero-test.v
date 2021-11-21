1239a9e82067f82e5d314bfe4cebd11e7dff1b2b
vonavi <vonavi@users.noreply.github.com>
2021-11-21 11:59:38 +0000

    deploy: 554ca613fa88ebc2cf1ad0810b810a32fe592bf3

---
+++ examples/contradiction-on-natural-numbers-zero-test.v
@@ -0,0 +1,92 @@
+(*|
+Contradiction on natural number's zero test
+===========================================
+
+:Link: https://stackoverflow.com/questions/58245731/contradiction-on-natural-numbers-zero-test
+|*)
+
+(*|
+Question
+--------
+
+I have a natural number that is not equal to zero. I want to prove
+that if it is equal to zero then it give false.
+|*)
+
+Require Import PeanoNat. (* .none *)
+Lemma notzero : forall n, n <> 0 -> (n =? 0) = false.
+Proof.
+  intros n H. Fail inversion H.
+
+(*|
+Answer (larsr)
+--------------
+
+Instead of using inversion in every proof, I find that the proofs are
+more maintainable in the long run if you use boolean reflection. The
+libraries have many useful lemmas that you can use, they often are
+called something with "reflect", "decide", "dec" or "spec" as in
+specification, so you can i.e. search for lemmas related to `<` with
+`Search (_ < _) "spec"`.
+
+The reflection lemmas are designed to at the same time 1) destruct the
+boolean term in your proof context, and 2) add the corresponding
+`Prop` to your context, making it easy to then use `lia`, `omega`,
+etc. to finish the proof.
+
+In your case, you can use `Nat.eqb_spec` (from `Require Import
+PeanoNat.`). Do
+|*)
+
+  destruct (Nat.eqb_spec n 0). Undo. (* .none *)
+
+(*|
+It will create two branches, in one `n ?= 0` is replaced with `true`
+and `n = 0` is added to the context, and in the other `n =? 0` is
+replaced with `false` and `n <> 0` is added to the context. It is now
+very easy to finish of the proof, as the first goal's context contains
+the contradiction `n = 0` and `n <> 0`, and the second goal is `false
+= false`. You can use the automation tactical `now`, so the complete
+proof is
+|*)
+
+  now destruct (Nat.eqb_spec n 0).
+
+(*|
+If you want to use integers `Z`, instead, the proof becomes `now
+destruct (Z.eq_spec n 0).`. as the `Z` module has many of the
+corresponding lemmas with matching names.
+|*)
+
+Qed. (* .none *)
+
+(*|
+Answer (Théo Winterhalter)
+--------------------------
+
+Generally, you can use the fact that `=?` reflects equality on natural
+numbers (`Nat.eqb_spec`). Are you using the two notions of equality on
+purpose? Note that `n <> 0` is a notation for `n = 0 -> False`.
+|*)
+
+Reset notzero. (* .none *)
+Lemma notzero : forall n, n <> 0 -> (n =? 0) = false.
+Proof.
+  intros n h. destruct (Nat.eqb_spec n 0).
+  - exfalso. apply h. assumption.
+  - reflexivity.
+Qed.
+
+(*|
+There is also the possibility of simply doing an analysis on `n`. If
+it is `0` then you can use your hypothesis to conclude, and if it is
+some `S m` then your goal will be provable by `reflexivity`.
+|*)
+
+Reset notzero. (* .none *)
+Lemma notzero : forall n, n <> 0 -> n =? 0 = false.
+Proof.
+  intros n h. destruct n.
+  - contradiction.
+  - reflexivity.
+Qed.
---
+++ examples/coq-proving-relation-between-and-≤.v
@@ -0,0 +1,109 @@
+(*|
+Coq: Proving relation between < and <=
+======================================
+
+:Link: https://stackoverflow.com/questions/57719818/coq-proving-relation-between-and-%e2%89%a4
+|*)
+
+(*|
+Question
+--------
+
+I am learning Coq right now and in a larger proof I have become
+stumped by the following sub-proof:
+|*)
+
+Theorem sub : forall n m : nat, n <= m -> n <> m -> n < m.
+Abort. (* .none *)
+
+(*| Or, once unfolded: |*)
+
+Theorem sub : forall n m : nat, n <= m -> n <> m -> S n <= m.
+Abort. (* .none *)
+
+(*| Here, `n <= m` is inductively defined as follows: |*)
+
+Inductive le : nat -> nat -> Prop :=
+| le_n : forall n : nat, le n n
+| le_S : forall n m : nat, le n m -> le n (S m).
+
+(*| I have not gotten very far, but my attempt looks like this: |*)
+
+Theorem sub : forall n m : nat, n <= m -> n <> m -> n < m.
+Proof.
+  unfold lt. intro n. induction n.
+  - induction m.
+    + intros. exfalso. contradiction.
+    + (* .unfold *)
+
+(*|
+I am not sure how I can leverage this hypothesis to prove the subgoal.
+I would appreciate any guidance in the right direction.
+|*)
+
+Admitted. (* .none *)
+
+(*|
+Answer
+------
+
+Since `le` is defined as an inductive predicate, it makes more sense
+to do the induction on that, rather than `n`. `le` doesn't make any
+references to `0` or even `S n` (it does have `S m`), so induction on
+`n` is probably not the way to go. Induction on `m` *might* work, but
+it's probably harder than necessary.
+
+Before starting a formal proof, it can often be helpful to think about
+how you would prove this informally (still using the same definitions,
+though). If you assume that `n <= m`, then by the inductive definition
+of `lt`, this means that either `n` and `m` are the same, or `m` is
+the successor of some number `m'` and `n` is less than or equal to
+`m'` (can you see why the definition of `lt` implies this?). In the
+first case, we'll have to use the additional hypothesis that `n <> m`
+to derive a contradiction. In the second case, we won't even need
+that. `n <= m'` implies that `S n <= S m'`, so since `m = S m'`, `S n
+<= m`, i.e., `n < m`.
+
+For the formalization, we'll have to prove that assumption on the last
+line that `n <= m` implies `S n <= S m`. You should try a similar
+informal analysis to prove it. Other than that, the informal proof
+should be straightforward to formalize. Case analysis on `H: n <= m`
+is just `destruct H.`.
+
+----
+
+One more thing. This isn't essential, but can often help in the long
+run. When defining an inductive type (or predicate), if you can factor
+out a parameter that's used the same way in each constructor, it can
+make the induction principle more powerful. The way you have it with
+`le`, `n` is universally quantified and used the same way for both
+constructors. Every instance of `le` starts with `le n`.
+|*)
+
+Reset le. (* .none *)
+Inductive le : nat -> nat -> Prop :=
+| le_n : forall n : nat, le n n
+| le_S : forall n m : nat, le n m -> le n (S m).
+
+(*| That means that we can factor out that index into a parameter. |*)
+
+Inductive le' (n: nat) : nat -> Prop :=
+| le_n' : le' n n
+| le_S' : forall m : nat, le' n m -> le' n (S m).
+
+(*| This gives you a slightly simpler/better induction principle. |*)
+
+Check le'_ind. (* .unfold .messages *)
+
+(*| Compare this to `le_ind`. |*)
+
+Check le_ind. (* .unfold .messages *)
+
+(*|
+Basically what's happening here is that with `le_ind`, you have to
+prove everything for every `n`. With `le'_ind`, you only need to prove
+it for the particular `n` that you're using. This can sometimes
+simplify proofs, though it's not necessary for the proof of your
+theorem. It's a good exercise to prove that these two predicates are
+equivalent.
+|*)
