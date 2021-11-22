750f241047f24c9a13f58e33110d78d78e735f25
Vladimir Ivanov <ivvl82@gmail.com>
2021-11-22 23:29:35 +0300

    Add four more answers from StackOverflow.

---
+++ examples/how-could-i-make-example-for-sigma-type-in-coq.v
@@ -0,0 +1,86 @@
+(*|
+How could I make example for sigma type in Coq?
+===============================================
+
+:Link: https://stackoverflow.com/questions/56365504/how-could-i-make-example-for-sigma-type-in-coq
+|*)
+
+(*|
+Question
+--------
+
+For that type:
+|*)
+
+Record Version :=
+  mkVersion {
+      major  : nat;
+      minor  : nat;
+      branch : {b : nat | b > 0 /\ b <= 9};
+      hotfix : {h : nat | h > 0 /\ h < 8}
+    }.
+
+(*| I'm trying to make an example: |*)
+
+Fail Example ex1 := mkVersion 3 2 (exist _ 5) (exist _ 5). (* .unfold *)
+
+(*| And it failed. What am I missing? |*)
+
+(*|
+Answer
+------
+
+The reason it fails is that you need not only provide a witness (`b`
+and `h` in this case) but also a proof that the corresponding
+condition holds for the provided witness.
+
+I would switch to booleans to make my life easier, because this allows
+proof by computation, which is basically what `eq_refl` does in the
+snippet below:
+|*)
+
+Reset Version. (* .none *)
+From Coq Require Import Bool Arith.
+
+Coercion is_true : bool >-> Sortclass.
+
+Record Version :=
+  mkVersion {
+      major  : nat;
+      minor  : nat;
+      branch : {b : nat | (0 <? b) && (b <=? 9)};
+      hotfix : {h : nat | (0 <? h) && (h <? 8)}
+    }.
+
+Example ex1 := mkVersion 3 2 (exist _ 5 eq_refl) (exist _ 5 eq_refl).
+
+(*|
+We could introduce a notation allowing a nicer representation of literals:
+|*)
+
+Notation "<| M ',' m ',' b '~' h |>" :=
+  (mkVersion M m (exist _ b eq_refl) (exist _ h eq_refl)).
+
+Example ex2 := <| 3,2,5~5 |>.
+
+(*|
+If there is a need to add manual proofs then I'd suggest to use
+`Program` mechanism:
+|*)
+
+From Coq Require Import Program.
+
+Program Definition ex3 b h (condb : b =? 5) (condh : h =? 1) :=
+  mkVersion 3 2 (exist _ b _) (exist _ h _).
+Next Obligation.
+  now unfold is_true in * |-; rewrite Nat.eqb_eq in * |-; subst. Qed.
+Next Obligation.
+  now unfold is_true in * |-; rewrite Nat.eqb_eq in * |-; subst. Qed.
+
+(*| or `refine` tactic: |*)
+
+Definition ex3' b h (condb : b =? 5) (condh : h =? 1) : Version.
+Proof.
+  now refine (mkVersion 3 2 (exist _ b _) (exist _ h _));
+    unfold is_true in * |-; rewrite Nat.eqb_eq in * |-; subst.
+Qed.
