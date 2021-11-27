d7e74c6f4f2f091e66870b4d38a4edfa5afceb40
vonavi <vonavi@users.noreply.github.com>
2021-11-27 15:57:42 +0000

    deploy: c216fbde43cd2fbb839c5a5d692e760dd22d6f51

---
+++ examples/lemma-as-a-type-in-a-record.v
@@ -0,0 +1,119 @@
+(*|
+###########################
+Lemma as a type in a record
+###########################
+
+:Link: https://stackoverflow.com/questions/55563771/lemma-as-a-type-in-a-record
+|*)
+
+(*|
+Question
+********
+
+Beginner here! How do we I interpret a record that looks something
+like this?
+|*)
+
+Require Import ssrbool. (* .none *)
+Fail Record test A B :=
+  {
+  CA: forall m, A m;
+  CB: forall a b m, CA m ==> B(a, b);
+  }. (* .fails *)
+
+(*|
+I am trying to get a sense of what an instance of this record would
+look like and moreover, what it means to have a quantified lemma as a
+type.
+|*)
+
+(*|
+Answer
+******
+
+What you are writing cannot make sense because the notation ``_ ==>
+_`` is supposed to link two boolean values. But ``CA`` has type ``A
+m``, which is itself a type, not a boolean value.
+
+One possibility to go forward would be to make ``CA`` a boolean
+function that could represent the ``A`` predicate.
+
+Another difficulty with your hypothetical record is that we don't know
+what are the input types for ``A`` and ``B``, so I will assume we live
+in an ambient type ``T`` over which quantifications appear. So here is
+a variant:
+|*)
+
+Record test (T : Type) (A : T -> Prop) (B : T * T -> bool) :=
+  {
+  CA : T -> bool;
+  CA_A : forall m, CA m = true -> A m;
+  CB : forall a b m, (CA m ==> B(a, b)) = true
+  }.
+
+(*|
+This example forces you to understand that there are two distinct
+concepts in this logical language: ``bool`` values and ``Prop``
+values. They represent different things, which can sometimes be
+amalgamated but you need to make the distinction clear in your head to
+leave the category of *beginner*.
+
+For your last sentence "what it means to have a quantified lemma as a
+type" here is another explanation.
+
+When programming with a conventional programming language, you can
+return arrays of integers. However, you cannot be explicit and say
+that you want to return an array of integers of a *specific length*.
+In Gallina (the basic programming language of Coq), you can define a
+type of arrays of length 10. Let us assume that such a type would be
+written ``array n``. So ``array 10`` and ``array 11`` would be two
+different types. A function that takes as input a number ``n`` and
+return as output an array of length ``n`` would have the following
+type:
+
+.. code-block:: coq
+
+    forall n, array n
+
+So an object that has a quantified formula as a type simply is a
+function.
+
+From a logical point of view, the statement ``forall n, array n`` is
+usually read as *for every* ``n`` *there exists an array of* ``size
+n``. This statement is probably no surprise to you.
+
+So the type of an array depends on an indice. Now we can think of
+another type, for example, the *type of proofs that* ``n`` *is prime*.
+Let's assume this type is written ``prime n``. Surely, there are
+numbers that are not prime, so for example the type ``prime 4`` should
+not contain any proof at all. Now I may write a function called
+``test_prime : nat -> bool`` with the property that when it returns
+``true`` I have the guarantee that the input is prime. This would be
+written as such:
+
+.. code-block:: coq
+
+    forall n, test_prime n = true -> prime n
+
+Now, if I want to define a collection of all correct prime testing
+functions, I would want to associate in one piece of data the function
+and the proof that it is correct, so I would define the following data
+type.
+|*)
+
+Parameter prime : nat -> Prop. (* .none *)
+Record certified_prime_test :=
+  {
+  test_prime : nat -> bool;
+  certificate : forall n, test_prime n = true -> prime n
+  }.
+
+(*|
+So records that contain universally quantified formulas can be in one
+of these two categories: either one component is one of this function
+whose output varies across several types of the same family (like in
+the example of ``array``) or one of the components actually brings
+more logical information about another component which is a function.
+In the ``certified_prime_test`` example, the ``certificate`` component
+brings more information about the ``test_prime`` function.
+|*)
---
+++ examples/structural-recursion-on-two-arguments.v
@@ -0,0 +1,110 @@
+(*|
+#####################################
+Structural recursion on two arguments
+#####################################
+
+:Link: https://stackoverflow.com/questions/55704472/structural-recursion-on-two-arguments
+|*)
+
+(*|
+Question
+********
+
+I have tried to make a function in Coq which has a pretty complex
+termination argument. To make it easier, I am able to write the
+function so that it has a natural number as first argument, so that
+either the number or the argument after it is structurally smaller.
+
+When trying the nested fix approach to recursion on two arguments, Coq
+complains that a proof argument that contains the semantics of the
+decreasing number is not an inductive type.
+
+I could probably do well-founded recursion manually, but I would like
+to use Program Fixpoint or Equations. With Program Fixpoint I get a
+very ugly version of the well-foundedness proof. Below is a minimal
+code example that demonstrates the ugliness.
+|*)
+
+Require Import Program.
+
+Inductive tuple_lt : (nat * nat) -> (nat * nat) -> Prop :=
+  fst_lt : forall a b c d, a < c -> tuple_lt (a, b) (c, d).
+
+Program Fixpoint f (a : nat) (b : nat) {measure (a, b) tuple_lt} :=
+  match a with
+  | 0 => 0
+  | S n => f n b
+  end.
+
+Next Obligation.
+  apply fst_lt. auto.
+Qed.
+
+Next Obligation.
+  unfold well_founded. unfold MR. (* .unfold *)
+Abort. (* .none *)
+
+(*|
+Can I somehow transform a proof of ``Acc tuple_lt`` into that ugly
+proof or avoid generating it?
+
+Is there a proof in the standard library for structural recursion on
+two arguments?
+
+How do I even write a manual WF proof using Equations? The manual does
+not mention that.
+|*)
+
+(*|
+Answer
+******
+
+In simple cases like this one, you shouldn't have to unfold
+definitions such as ``well_founded`` and ``MR``, but rather use
+appropriate lemmas.
+
+To deal with ``MR``, you can use lemma ``measure_wf`` in Program.Wf.
+
+To prove the well-foundedness of ``tuple_lt``, you can rely on lemmas
+showing the well-foundedness of a relation based on the
+well-foundedness of another relation. Here, we can use
+``well_founded_lt_compat``. In other cases, you may find other lemmas
+useful, such as ``wf_inverse_image``, ``well_founded_ltof`` or
+``well_founded_gtof``.
+
+The proof of the well-foundedness of ``tuple_lt`` becomes simple.
+|*)
+
+Require Import Coq.Arith.Wf_nat. (* .none *)
+Lemma tuple_lt_wf : well_founded tuple_lt.
+Proof.
+  apply well_founded_lt_compat with fst.
+  intros ? ? []; assumption.
+Defined.
+
+(*| And so does the proof of the second obligation. |*)
+
+Next Obligation.
+  apply measure_wf. apply tuple_lt_wf.
+Defined.
+
+(*|
+(Note that in both cases, you should end the proofs with ``Defined``
+instead of ``Qed``, if you want your function defined by ``Program
+Fixpoint`` to compute inside Coq (otherwise it gets stuck on opaque
+proofs); it seems that you can end the proof of the first obligation
+with ``Qed``, though).
+
+You could also use the following simpler definition for ``tuple_lt``:
+|*)
+
+Reset tuple_lt. (* .none *)
+Require Import Coq.Arith.Wf_nat. (* .none *)
+Definition tuple_lt (p1 p2 : nat * nat) := fst p1 < fst p2.
+
+(*| In that case, the proof of well-foundedness is trivial. |*)
+
+Lemma tuple_lt_wf : well_founded tuple_lt.
+Proof.
+  apply well_founded_ltof.
+Defined.
